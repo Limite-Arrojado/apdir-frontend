95b076b4cddaf0721f86801763b5f9f8ddeb26df
limitearrojado <145594664+limitearrojado@users.noreply.github.com>
2023-09-26 20:48:25 +0100

    added events section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Banner from "@/components/pages/home/Banner";
+import Events from "@/components/pages/home/Events";
 import Insol from "@/components/pages/home/Insol";
 import News from "@/components/pages/home/News";
 
-export default async function Home() {
+export default function Home() {
   return (
     <>
       <Banner />
       <Insol />
       <News />
+      <Events />
     </>
   );
 }
